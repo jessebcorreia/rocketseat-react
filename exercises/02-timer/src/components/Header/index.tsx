2f9fa628a091f6567f873826271fd76bf6b42a3f
= <jesse.bcorreia@gmail.com>
2024-08-02 23:27:20 -0300

    /exercises/02-timer | continuando a estruturação das páginas e rotas
    Desenvolvimento da estrutura base das páginas Home e History, e outros ajustes no layout

---
+++ exercises/02-timer/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
--- exercises/02-timer/src/pages/Home/index.tsx
+++ exercises/02-timer/src/pages/Home/index.tsx
@@ -1,7 +1,56 @@
+import { Play } from 'phosphor-react'
+import {
+  HomeContainer,
+  FormContainer,
+  CountdownContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles'
+
 export function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Sugestão 1" />
+            <option value="Sugestão 2" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
   )
 }
